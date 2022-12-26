20ad117c568c190944171774e7aaec3fbba00736
lonely <lonely@qq.com>
2022-12-26 20:32:23 +0800

    改为post

--- mental-api/src/main/java/com/mental/controller/LoginController.java
+++ mental-api/src/main/java/com/mental/controller/LoginController.java
@@ -5,10 +5,7 @@ import com.mental.pojo.Student;
 import com.mental.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -21,7 +18,7 @@ public class LoginController {
      * 学生登陆
      * @return
      */
-    @GetMapping("/student")
+    @PostMapping("/student")
     public Student studentLogin(@RequestBody Student student){
         Student student1 = studentService.login(student);
 
