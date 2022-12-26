27faf0df09e98d3efb0d14807682c134aaba2b29
lonely <lonely@qq.com>
2022-12-26 21:21:43 +0800

    修改student

--- mental-api/src/main/java/com/mental/common/Result.java
+++ mental-api/src/main/java/com/mental/common/Result.java
@@ -8,37 +8,15 @@ import java.util.Map;
 
 /**
  * 所有响应返回结果类
- * @param <T>
+ * @param
  */
 
 @Data
-public class Result<T> implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Result{
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    private Integer code; //编码：200成功，400和其它数字为失败
 
-    private String msg; //错误信息
-
-    private T data; //数据
-
-    private Map map = new HashMap(); //动态数据
-
-    public static <T> Result<T> success(T object) {
-        Result<T> result = new Result<T>();
-        result.data = object;
-        result.code = 1;
-        return result;
-    }
-
-    public static <T> Result<T> error(String msg) {
-        Result result = new Result();
-        result.msg = msg;
-        result.code = 0;
-        return result;
-    }
-
-    public Result<T> add(String key, Object value) {
-        this.map.put(key, value);
-        return this;
+    public Result(Integer code){
+        this.code = code;
     }
 }
--- mental-api/src/main/java/com/mental/controller/LoginController.java
+++ mental-api/src/main/java/com/mental/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.mental.controller;
 
 import com.mental.common.Result;
+import com.mental.pojo.ResultCode;
 import com.mental.pojo.Student;
 import com.mental.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,9 +20,16 @@ public class LoginController {
      * @return
      */
     @PostMapping("/student")
-    public Student studentLogin(@RequestBody Student student){
-        Student student1 = studentService.login(student);
+    public Result studentLogin(@RequestBody Student student){
+        try {
+            Student stu = studentService.login(student);
+            if(stu == null){
+                return new Result(ResultCode.ERROR);
+            }
+        } catch (Exception e) {
+            return new Result(ResultCode.ERROR);
+        }
 
-        return student1;
+        return new Result(ResultCode.SUCCESS);
     }
 }
